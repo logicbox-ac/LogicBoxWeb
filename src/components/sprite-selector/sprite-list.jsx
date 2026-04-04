4604afce638253569d2d4282c89b1895ff0565b9
Ubuntu <ubuntu@ip-172-31-31-102.ec2.internal>
2026-04-04 15:30:22 +0000

    Fix mobile costume canvas and sprite menus

--- src/components/sprite-selector/sprite-list.jsx
+++ src/components/sprite-selector/sprite-list.jsx
@@ -37,77 +37,79 @@ const SpriteList = function (props) {
     const isSpriteDrag = draggingType === DragConstants.SPRITE;
 
     return (
-        <Box
-            className={classNames(styles.scrollWrapper, {
-                [styles.scrollWrapperDragging]: draggingType === DragConstants.BACKPACK_SPRITE
-            })}
-            componentRef={containerRef}
-        >
+        <React.Fragment>
             <Box
-                className={styles.itemsWrapper}
-            >
-                {items.map((sprite, index) => {
-
-                    // If the sprite has just received a block drop, used for green highlight
-                    const receivedBlocks = (
-                        hoveredTarget.sprite === sprite.id &&
-                    sprite.id !== editingTarget &&
-                    hoveredTarget.receivedBlocks
-                    );
-
-                    // If the sprite is indicating it can receive block dropping, used for blue highlight
-                    let isRaised = !receivedBlocks && raised && sprite.id !== editingTarget;
-
-                    // A sprite is also raised if a costume or sound is being dragged.
-                    // Note the absence of the self-sharing check: a sprite can share assets with itself.
-                    // This is a quirk of 2.0, but seems worth leaving possible, it
-                    // allows quick (albeit unusual) duplication of assets.
-                    isRaised = isRaised || [
-                        DragConstants.COSTUME,
-                        DragConstants.SOUND,
-                        DragConstants.BACKPACK_COSTUME,
-                        DragConstants.BACKPACK_SOUND,
-                        DragConstants.BACKPACK_CODE].includes(draggingType);
-
-                    return (
-                        <SortableAsset
-                            className={classNames(styles.spriteWrapper, {
-                                [styles.placeholder]: isSpriteDrag && index === draggingIndex})}
-                            index={isSpriteDrag ? ordering.indexOf(index) : index}
-                            key={sprite.name}
-                            onAddSortable={onAddSortable}
-                            onRemoveSortable={onRemoveSortable}
-                        >
-                            <ThrottledSpriteSelectorItem
-                                asset={sprite.costume && sprite.costume.asset}
-                                className={classNames(styles.sprite, {
-                                    [styles.raised]: isRaised,
-                                    [styles.receivedBlocks]: receivedBlocks
-                                })}
-                                dragPayload={sprite.id}
-                                dragType={DragConstants.SPRITE}
-                                id={sprite.id}
-                                index={index}
-                                key={sprite.id}
-                                name={sprite.name}
-                                selected={sprite.id === selectedId}
-                                onClick={onSelectSprite}
-                                onDeleteButtonClick={onDeleteSprite}
-                                onDuplicateButtonClick={onDuplicateSprite}
-                                onExportButtonClick={onExportSprite}
-                                withDeleteConfirmation
-                                deleteConfirmationModalPosition={'left'}
-                            />
-                        </SortableAsset>
-                    );
+                className={classNames(styles.scrollWrapper, {
+                    [styles.scrollWrapperDragging]: draggingType === DragConstants.BACKPACK_SPRITE
                 })}
-                {addButton ? (
-                    <Box className={styles.addButtonTray}>
-                        {addButton}
-                    </Box>
-                ) : null}
+                componentRef={containerRef}
+            >
+                <Box
+                    className={styles.itemsWrapper}
+                >
+                    {items.map((sprite, index) => {
+
+                        // If the sprite has just received a block drop, used for green highlight
+                        const receivedBlocks = (
+                            hoveredTarget.sprite === sprite.id &&
+                        sprite.id !== editingTarget &&
+                        hoveredTarget.receivedBlocks
+                        );
+
+                        // If the sprite is indicating it can receive block dropping, used for blue highlight
+                        let isRaised = !receivedBlocks && raised && sprite.id !== editingTarget;
+
+                        // A sprite is also raised if a costume or sound is being dragged.
+                        // Note the absence of the self-sharing check: a sprite can share assets with itself.
+                        // This is a quirk of 2.0, but seems worth leaving possible, it
+                        // allows quick (albeit unusual) duplication of assets.
+                        isRaised = isRaised || [
+                            DragConstants.COSTUME,
+                            DragConstants.SOUND,
+                            DragConstants.BACKPACK_COSTUME,
+                            DragConstants.BACKPACK_SOUND,
+                            DragConstants.BACKPACK_CODE].includes(draggingType);
+
+                        return (
+                            <SortableAsset
+                                className={classNames(styles.spriteWrapper, {
+                                    [styles.placeholder]: isSpriteDrag && index === draggingIndex})}
+                                index={isSpriteDrag ? ordering.indexOf(index) : index}
+                                key={sprite.name}
+                                onAddSortable={onAddSortable}
+                                onRemoveSortable={onRemoveSortable}
+                            >
+                                <ThrottledSpriteSelectorItem
+                                    asset={sprite.costume && sprite.costume.asset}
+                                    className={classNames(styles.sprite, {
+                                        [styles.raised]: isRaised,
+                                        [styles.receivedBlocks]: receivedBlocks
+                                    })}
+                                    dragPayload={sprite.id}
+                                    dragType={DragConstants.SPRITE}
+                                    id={sprite.id}
+                                    index={index}
+                                    key={sprite.id}
+                                    name={sprite.name}
+                                    selected={sprite.id === selectedId}
+                                    onClick={onSelectSprite}
+                                    onDeleteButtonClick={onDeleteSprite}
+                                    onDuplicateButtonClick={onDuplicateSprite}
+                                    onExportButtonClick={onExportSprite}
+                                    withDeleteConfirmation
+                                    deleteConfirmationModalPosition={'left'}
+                                />
+                            </SortableAsset>
+                        );
+                    })}
+                </Box>
             </Box>
-        </Box>
+            {addButton ? (
+                <Box className={styles.addButtonTray}>
+                    {addButton}
+                </Box>
+            ) : null}
+        </React.Fragment>
     );
 };
 
